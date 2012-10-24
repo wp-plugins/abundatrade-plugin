b92313f7612c79e05120db94a3a574a574e22a4b
withinboredom <withinboredom@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-10-24 18:58:33 +0000

    v0.1
    git-svn-id: https://plugins.svn.wordpress.org/abundatrade-plugin/tags/0.1@616978 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ skel/skel.php
@@ -0,0 +1,95 @@
+<?php
+/**
+ * SKEL is a ui managment library
+ * @author Robert Landers (landers.robert@gmail.com)
+ * @copyright 2012 GPLV2+
+ * @package Abundatrade
+ * @subpackage skel
+ * @version 0.1
+ */
+
+if (!defined("WP_CONTENT_DIR")) exit();
+
+class skel__skel
+{
+    /**
+     * The configuration of skel stored
+     */
+    private $config;
+    
+    /**
+     * The folder list
+     */
+    private $folders;
+    
+    /**
+     * Lays out the skel framework
+     */
+	function __construct()
+    {
+    	$this->config = apply_filters("abundatrade(applyConfig)", array());
+        $this->folders = apply_filters("abundatrade(getFolders)", array());
+        
+        if (isset($this->config['config'])) {
+            if (isset($this->config['config']['settings']) && $this->config['config']['settings']) {
+                if (is_admin()) {
+                    
+                    //now we add a settings menu item
+                    add_action('admin_menu', array($this, 'buildSettings'));
+                    add_action('admin_print_styles',array($this, 'loadcss'));
+                }
+            }
+        }
+    }
+    
+    /**
+     * Loads tabified css
+     */
+    public function loadcss() {
+        wp_register_style('abunda_admin_css', $this->folders['PluginUrl'] .'/css/admin.css'); 
+        wp_enqueue_style ('abunda_admin_css');
+    }
+    
+    /**
+     * Displays the admin page for a given tab, and defaults to tab 0
+     * Calls stuff
+     */
+    public function display() {
+        if(!isset($_GET['tab']))
+        {
+            $_GET['tab'] = 0;
+        }
+        
+        if (current_user_can('manage_options')) $this->render_tabs();
+    }
+    
+    private function render_tabs() {
+        ?>
+        <div id="icon-MoneyPress_Abundatrade_Edition" class="icon32"><br /></div><h2>Abundatrade Settings</h2>
+        <div id="navcontainer">
+            <ul id="navlist">
+                <?php
+        $count = 0;
+        foreach ($this->config['tabs'] as $tab) {
+            echo "<li><a" . (($tab[0] === $this->config['tabs'][$_GET['tab']][0]) ? " id='current'": "") . " href='". $this->folders['PluginAdmin'] ."&tab=". $count++ ."'>" . $tab[0] . "</a></li>";
+        }
+                ?>
+            </ul>
+        </div>
+        <div class="tool-box">
+        <h3 class="title"><?php echo $this->config['tabs'][$_GET['tab']][0]; ?></h3>
+        <?php
+        $display = new $this->config['tabs'][$_GET['tab']][1];
+        $display->settings();
+        ?>
+        </div>
+        <?php
+    }
+    
+    /**
+     * Builds the settings menu stuff
+     */
+    public function buildSettings() {
+        add_options_page($this->config['config']['page_title'], $this->config['config']['button_title'], 'manage_options', $this->config['config']['slug'], array($this, "display"));
+    }
+}
---
+++ tabs/settings_withinboredom.php
@@ -0,0 +1,64 @@
+<?php
+/**
+ * Displays a settings page
+ * @author Robert Landers (landers.robert@gmail.com)
+ * @package abundatrade
+ * @version 1.0
+ */
+
+//do not allow hijackers
+if (!defined("WP_CONTENT_DIR")) exit();
+
+/**
+ * This class handles display of all options
+ */
+class tabs__settings_withinboredom
+{
+	function __construct() {
+        
+    }
+    
+    // display admin stuff ...
+    function settings() {
+        $settings = skel__settings::Settings();
+        
+        if (current_user_can('manage_options')) {
+            $settings->applySettings($_POST);
+        }
+        
+        ?>
+        <form method="post" action="">
+        <div id="poststuff" class="metabox-holder">
+        <div class="meta-box-sortables">
+        <script type="text/javascript">
+        
+         jQuery(document).ready(function($) {
+             $('.postbox').children('h3, .handlediv').click(function(){
+                 $(this).siblings('.inside').toggle();
+             });
+         });         
+         jQuery(document).ready(function($) {
+             $('.csl_themes-moreicon').click(function(){
+                 $(this).siblings('.csl_themes-moretext').toggle();
+             });
+         });         
+       </script>
+       
+        <?php
+        skel__settings::RenderBox("Using", "Place the [abundatrade] shortcode on any page or post where you want the Abundatrade calculator to appear.");
+        skel__settings::RenderBox("Affiliate Information", "Enter your affiliate Id", array(
+            'Affiliate ID' => array(
+                    'type'  => 'text',
+                    'name'  => 'Affiliate ID',
+                    'id'    => 'Affiliate_ID',
+                    'value' => $settings->Affiliate_ID
+                )
+            ));
+        ?>
+        </div>
+        </div>
+        <input type="submit" class="button-primary" value="Save Changes" />
+        </form>
+        <?php
+    }
+}
