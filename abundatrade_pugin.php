c95bfa5be946485560dd77469c2194a3a8679e78
withinboredom <withinboredom@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-12-08 20:52:38 +0000

    v1.5
    git-svn-id: https://plugins.svn.wordpress.org/abundatrade-plugin/tags/1.5@635933 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- abundatrade_pugin.php
+++ abundatrade_pugin.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @package abundatrade_plugin
- * @version 1.3
+ * @version 1.5
  * @author Robert Landers (landers.robert@gmail.com)
  */
 /*
@@ -9,7 +9,7 @@ Plugin Name: Abundatrade Plugin
 Plugin URI: http://wordpress.org/extend/plugins/abundatrade-plugin/
 Description: Earn extra income for your site via the Abundatrade affiliate program!
 Author: withinboredom
-Version: 1.3
+Version: 1.5
 Author URI: http://withinboredom.info
  */
 
@@ -161,13 +161,15 @@ class abundatrade_withinboredom {
     public function addScripts() {
         wp_register_style("abundatrade_classic", $this->folders['PluginUrl'] . '/themes/' . $this->settings->Theme . '.css');
         wp_register_script("abundatrade_md5", $this->folders['PluginUrl'] . '/js/MD5.js');
-        wp_register_script("abundatrade_remote", $this->folders['PluginUrl'] . '/js/remote.js', array('jquery','abundatrade_md5'));
+        wp_register_script("abundatrade_remote", $this->folders['PluginUrl'] . '/js/remote.min.js', array('jquery','abundatrade_md5'));
         wp_register_script("abundatrade_impromptu", $this->folders['PluginUrl'] . '/js/jquery-impromptu.4.0.min.js', array('jquery'));
+        wp_register_script("abundatrade_register", $this->folders['PluginUrl'] . '/js/register.min.js', array('jquery', 'abundatrade_remote'));
         
         wp_enqueue_style("abundatrade_classic");
         wp_enqueue_script("abundatrade_md5");
         wp_enqueue_script("abundatrade_remote");
         wp_enqueue_script("abundatrade_impromptu");
+        wp_enqueue_script("abundatrade_register");
         $abundacalc = array('server' => 'abundatrade.com', 
             'url' => $this->folders['PluginUrl'], 
             'thanks' => $this->settings->Thank_you_page);
